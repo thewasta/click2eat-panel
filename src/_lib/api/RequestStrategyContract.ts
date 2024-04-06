1d5786870d1392493f5b6b28cb22c47b4ed351f2
Schenier Lopez <schnierlopez@gmail.com>
2024-04-06 22:27:18 +0200

    :construction: feat: Remove API manage

--- src/_lib/api/RequestStrategyContract.ts
+++
@@ -1,5 +0,0 @@
-export type Request_Type = 'GET' | 'POST' | 'DELETE' | 'PUT';
-
-export interface RequestStrategy {
-    request(endpoint: string, method: Request_Type, body?: {}, headers?: HeadersInit): Promise<any>;
-}
