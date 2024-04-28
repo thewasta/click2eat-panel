fdb32b51fd1062ad06b4252817cd59ca97df9265
Schenier Lopez <schnierlopez@gmail.com>
2024-04-28 20:46:59 +0200

    :triangular_flag_on_post: Add playwright e2e testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,11 +20,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'html' : 'line',
+  reporter: [
+    ['html', { outputFolder: 'test-results' }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://click2eat.localhost',
+    baseURL: 'http://127.0.0.1:3000/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -69,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:3000/',
+    reuseExistingServer: !process.env.CI,
+  },
 });
