7752c633da87c3dcbc8a3914a7b95c5467231fbb
Schenier Lopez <schnierlopez@gmail.com>
2024-05-12 16:54:11 +0200

    :bug: postalCode validation

--- src/components/auth/RegisterBusiness.tsx
+++ src/components/auth/RegisterBusiness.tsx
@@ -13,7 +13,10 @@ const RegisterBusinessForm = () => {
         businessName: z.string({required_error: 'businessName is required'}).min(1),
         document: z.string({required_error: 'document is required'}).min(1),
         address: z.string({required_error: 'address is required'}).min(1),
-        postalCode: z.preprocess((a) => parseInt(z.string().parse(a), 10), z.number({required_error: 'postalCode is required'}).min(4)),
+        postalCode: z.coerce.number({
+            required_error: 'postalCode is required',
+            invalid_type_error: 'postalCode is required'
+        }).min(4),
         province: z.string({required_error: 'province is required'}).min(1),
         town: z.string({required_error: 'town is required'}).min(1),
         country: z.string({required_error: 'country is required'}).min(1),
