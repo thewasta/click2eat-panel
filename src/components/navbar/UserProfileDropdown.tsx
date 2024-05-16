8ce5a7ac3604983ca2d67b239c9627b61239de61
Schenier Lopez <schnierlopez@gmail.com>
2024-05-16 19:13:35 +0200

    :lipstick: Header menu add dropdown menu

---
+++ src/components/navbar/UserProfileDropdown.tsx
@@ -0,0 +1,41 @@
+'use client'
+import {
+    DropdownMenu,
+    DropdownMenuContent, DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel, DropdownMenuSeparator,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {Button} from "@/components/ui/button";
+import {User} from "lucide-react";
+import {logout} from "@/_request/auth/auth";
+import {useRouter} from "next/navigation";
+
+export function UserProfileDropdown() {
+    const router = useRouter()
+    return (
+      <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                  <User />
+              </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+              <DropdownMenuLabel>Perfil</DropdownMenuLabel>
+              <DropdownMenuSeparator/>
+              <DropdownMenuGroup>
+                  <DropdownMenuItem>
+                      Ajustes
+                  </DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator/>
+              <DropdownMenuItem onClick={async () => {
+                  await logout()
+                  router.replace('/');
+              }}>
+                  Salir
+              </DropdownMenuItem>
+          </DropdownMenuContent>
+      </DropdownMenu>
+    );
+}
