04d089d377d39e6c5ec5ad951185bd2d855a23d3
Schenier Lopez <schnierlopez@gmail.com>
2024-05-17 18:49:01 +0200

    Remove console.log

--- src/_request/product/create.ts
+++ src/_request/product/create.ts
@@ -5,9 +5,8 @@ import {revalidatePath} from "next/cache";
 
 export async function create(form: FormData) {
     try {
-        console.log('creat product');
         const ENDPOINT = 'api/v1/products';
-        const response = await requestWithSession(ENDPOINT, 'POST', form);
+        await requestWithSession(ENDPOINT, 'POST', form);
         revalidatePath('/dashboard/home/menu');
     }catch (e) {
         console.error(e);
