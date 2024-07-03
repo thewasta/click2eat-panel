a7f265bd1d0cb129fffc9acc647529b82e53fb1e
Schenier Lopez <schnierlopez@gmail.com>
2024-07-03 20:29:46 +0200

    :tada: Home dashboard

---
+++ src/app/dashboard/components/shopNameComponent.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import {createClient} from "@/lib/supabase/client";
+import {useEffect, useState} from "react";
+
+export default function ShopNameComponent() {
+    const supabase = createClient();
+    const [business, setBusiness] = useState<null | any>();
+
+    useEffect(() => {
+        const business = async () => {
+            const {data} = await supabase.from('business')
+                .select('*,business_user_pivot!inner(user_id)');
+            return data;
+        }
+        business().then(business => {
+            if (business) {
+                setBusiness(business[0].name);
+            }
+        }).catch(console.error);
+
+    }, []);
+
+    return (
+        <span className={'underline'}>
+            {business ?? 'NO DATA'}
+        </span>
+    )
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,23 @@
-import {createClient} from "@/lib/supabase/client";
+import {createClient} from "@/lib/supabase/server";
+import ShopNameComponent from "@/app/dashboard/components/shopNameComponent";
 
-export default function HomeDashboard() {
+export default async function HomeDashboard() {
     // const responsiveBreakpoint: string = "sm:bg-indigo-500 md:bg-red-600 lg:bg-green-300 xl:bg-blue-600 2xl:bg-red-800 bg-violet-600";
     const client = createClient();
-    const categories = null; //await client.from('product').select('*,category(*),sub_category(*)');
+    const {data: {user}} = await client.auth.getUser();
 
     return (
-        <>
-            <div className="col-span-2">
+        <div className="col-span-2">
                         <span className="font-bold text-xl">
-                            Buenos días, [].
+                            Buenos días, {user?.user_metadata.full_name}.
                         </span>
-                <p className="text-gray-500 text-sm">
-                    Esto es lo que está sucediendo hoy en <span className="underline">[Nombre tienda]</span>
-                </p>
-                <div className="grid grid-cols-3">
-                    {
-                        JSON.stringify(categories,null,2)
-                    }
-                </div>
+
+            <p className="text-gray-500 text-sm">
+                Esto es lo que está sucediendo hoy en <ShopNameComponent/>
+
+            </p>
+            <div className="grid grid-cols-3">
             </div>
-        </>
+        </div>
     )
 }
