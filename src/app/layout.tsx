f401dd4b8f85a7b1ede89a7244381c3356e150fb
Schenier Lopez Uriarte <schnierlopez@gmail.com>
2024-08-28 20:11:29 +0200

    feature/project/supabase-refactor-categories Fix optimize google font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,12 @@ import {UserAppContextProvider} from "@/lib/context/auth/user-context";
 import {ReactNode} from "react";
 import {Toaster} from "@/components/ui/sonner";
 
-const inter = Roboto({weight: ['100', '300', '400','500','700','900'],subsets:['latin']});
+const inter = Roboto({
+    weight: ['100', '300', '400', '500', '700', '900'],
+    subsets: ['latin'],
+    display: "swap",
+    adjustFontFallback: false
+},);
 
 export const metadata: Metadata = {
     title: {
