8a28ae05f7c876842e0a9dc0c8522c0c71b97ec2
Schenier Lopez Uriarte <schnierlopez@gmail.com>
2024-09-14 18:15:37 +0200

    feature/project/products-supabase-integration Rename to prevent eslint use hook error

--- src/_lib/_hooks/formDateFromUtc.ts
+++ src/_lib/_hooks/formDateFromUtc.ts
@@ -1,6 +1,6 @@
 import {format, toZonedTime} from 'date-fns-tz';
 
-export const useFormattedDate = (dateString: string) => {
+export const formDateFromUtc = (dateString: string) => {
     const utcDate = new Date(dateString + 'Z');
     const zonedDate = toZonedTime(utcDate, Intl.DateTimeFormat().resolvedOptions().timeZone);
     return format(zonedDate, 'yyyy-MM-dd HH:mm:ss', {timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone});
