0f5ce8e361afc7c542a34ffbae109c88f497eb19
Schenier Lopez Uriarte <schnierlopez@gmail.com>
2024-09-14 10:31:50 +0200

    feature/project/products-supabase-integration Variants scroll area

--- src/components/form/product/productFormVariants.tsx
+++ src/components/form/product/productFormVariants.tsx
@@ -8,6 +8,8 @@ import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {Input} from "@/components/ui/input";
 import {Switch} from "@/components/ui/switch";
 import {Label} from "@/components/ui/label";
+import {ScrollArea} from "@/components/ui/scroll-area";
+import {FormDescription} from "@/components/ui/form";
 
 type Variant = {
     name: string;
@@ -85,54 +87,65 @@ export function ProductFormVariants({variantGroups, setVariantGroups}: ProductFo
                         Crea una (o varias) variación para tu producto
                     </SheetDescription>
                 </SheetHeader>
-                <div className={'space-y-3'}>
-                    {variantGroups.map((group, groupIndex) => (
-                        <Card key={groupIndex}>
-                            <CardHeader>
-                                <CardTitle className={'space-y-1.5'}>
-                                    <Label htmlFor={`group-${groupIndex}`}>Nombre grupo</Label>
-                                    <Input
-                                        id={`group-${groupIndex}`}
-                                        value={group.name}
-                                        onChange={event => handleGroupChange(groupIndex, event)}
-                                        placeholder="Masa, Cocción..."
-                                    />
-                                </CardTitle>
-                            </CardHeader>
-                            <CardContent>
-                                {group.variants.map((variant, variantIndex) => (
-                                    <div key={variantIndex} className="flex flex-col space-y-2 mb-2">
-                                        <Input
-                                            name="name"
-                                            value={variant.name}
-                                            onChange={event => handleVariantChange(groupIndex, variantIndex, 'name', event.target.value)}
-                                            placeholder="Fina, Al punto...."
-                                        />
-                                        <Input
-                                            name="price"
-                                            value={variant.price}
-                                            onChange={event => handleVariantChange(groupIndex, variantIndex, 'price', event.target.value)}
-                                            placeholder="Precio"
-                                            type="number"
-                                        />
-                                        <div className="flex items-center space-x-2">
-                                            <Switch
-                                                checked={variant.isRequired}
-                                                onCheckedChange={(checked) => handleVariantChange(groupIndex, variantIndex, 'isRequired', checked)}/>
-                                            <Label htmlFor="airplane-mode">Requerido</Label>
-                                            <Button type="button" variant="ghost"
-                                                    onClick={() => removeVariant(groupIndex, variantIndex)}>
-                                                <Trash2 className="h-4 w-4"/>
-                                            </Button>
-                                        </div>
-                                    </div>
-                                ))}
-                                <Button type="button" onClick={() => addVariant(groupIndex)} className="mt-2">
-                                    <PlusCircle className="h-4 w-4 mr-2"/> Añadir Variante
-                                </Button>
-                            </CardContent>
-                        </Card>
-                    ))}
+                <div className={'h-full grid grid-rows-[1fr_100px]'}>
+                    <ScrollArea className={'p-3'}>
+                        <div className={'space-y-3'}>
+                            {variantGroups.map((group, groupIndex) => (
+                                <Card key={groupIndex}>
+                                    <CardHeader>
+                                        <CardTitle className={'space-y-1.5'}>
+                                            <Label htmlFor={`group-${groupIndex}`}>Nombre grupo</Label>
+                                            <Input
+                                                id={`group-${groupIndex}`}
+                                                value={group.name}
+                                                onChange={event => handleGroupChange(groupIndex, event)}
+                                                placeholder="Masa, Cocción..."
+                                            />
+                                        </CardTitle>
+                                    </CardHeader>
+                                    <CardContent>
+                                        {group.variants.map((variant, variantIndex) => (
+                                            <div key={variantIndex} className="flex flex-col space-y-2 mb-2">
+                                                <Input
+                                                    name="name"
+                                                    value={variant.name}
+                                                    onChange={event => handleVariantChange(groupIndex, variantIndex, 'name', event.target.value)}
+                                                    placeholder="Fina, Al punto...."
+                                                />
+                                                <Input
+                                                    name="price"
+                                                    value={variant.price}
+                                                    onChange={event => handleVariantChange(groupIndex, variantIndex, 'price', event.target.value)}
+                                                    placeholder="Precio"
+                                                    type="number"
+                                                />
+                                                <FormDescription>
+                                                    Coste extra por unidad
+                                                </FormDescription>
+                                                <div className="flex items-center space-x-2">
+                                                    <Switch
+                                                        checked={variant.isRequired}
+                                                        onCheckedChange={(checked) => handleVariantChange(groupIndex, variantIndex, 'isRequired', checked)}/>
+                                                    <Label htmlFor="airplane-mode">Requerido</Label>
+                                                    <Button type="button" variant="ghost"
+                                                            onClick={() => removeVariant(groupIndex, variantIndex)}>
+                                                        <Trash2 className="h-4 w-4"/>
+                                                    </Button>
+                                                </div>
+                                            </div>
+                                        ))}
+                                        <Button type="button" onClick={() => addVariant(groupIndex)} className="mt-2">
+                                            <PlusCircle className="h-4 w-4 mr-2"/> Añadir Variante
+                                        </Button>
+                                        <Button size={'icon'} type={'button'} variant={'ghost'}
+                                        onClick={() => removeVariantGroup(groupIndex)}>
+                                            <Trash2 className="h-4 w-4"/>
+                                        </Button>
+                                    </CardContent>
+                                </Card>
+                            ))}
+                        </div>
+                    </ScrollArea>
                     <Button type="button" onClick={addVariantGroup} className="mt-2">
                         <PlusCircle className="h-4 w-4 mr-2"/> Añadir Grupo de Variantes
                     </Button>
