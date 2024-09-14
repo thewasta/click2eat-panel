cf125fe8e445a57c8c8cbac267160e5b732cd91f
Schenier Lopez Uriarte <schnierlopez@gmail.com>
2024-09-14 10:31:40 +0200

    feature/project/products-supabase-integration Remove imports

--- src/components/form/product/productForm.tsx
+++ src/components/form/product/productForm.tsx
@@ -9,7 +9,7 @@ import {zodResolver} from "@hookform/resolvers/zod";
 import {CreateProductDTO, createProductSchema} from "@/_lib/dto/productFormDto";
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select";
 import {Textarea} from "@/components/ui/textarea";
-import {ChangeEvent, useState} from "react";
+import {ChangeEvent, useEffect, useState} from "react";
 import {Switch} from "@/components/ui/switch";
 import {toast} from "sonner";
 import {Tables} from "@/types/database/database";
@@ -145,8 +145,6 @@ export default function ProductForm<T>({product, categories, isLoading, isProduc
     });
 
     const ingredients = form.watch('ingredients');
-    const subCategoriesInput = form.watch('subCategory');
-    const categoriesInput = form.watch('category');
 
     const handleAddIngredient = (newIngredient: string) => {
         if (ingredients === undefined) return;
