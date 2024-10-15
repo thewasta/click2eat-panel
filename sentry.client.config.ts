47226dfe774e48033e9719bd27c98ec5e2ab8500
Schenier Lopez Uriarte <schnierlopez@gmail.com>
2024-10-16 01:42:10 +0200

    clean up environment variables and set types

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,9 +10,9 @@ Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Add optional integrations for additional features
-  integrations: [
-    Sentry.replayIntegration(),
-  ],
+  integrations: (process.env.SENTRY_ENABLE && process.env.SENTRY_DEBUG) ? [
+    Sentry.replayIntegration()
+  ] : [],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
@@ -26,5 +26,5 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: process.env.NODE_ENV !== "production",
+  debug: process.env.SENTRY_DEBUG,
 });
